f49274a19fba4aa0e00eeb05452499b8064eccac
IGamesI <69199901+IGamesI@users.noreply.github.com>
2023-05-03 21:15:04 +0330

    Started Working on Project Stocks

--- app.py
+++ app.py
@@ -6,10 +6,9 @@ app = Flask(__name__)
 # Load the json file with the student scores
 with open("student_scores.json", "r", encoding="utf-8") as f:
     student_scores = json.load(f)
-
-# Load the json file with the project stocks
-with open("project_stocks.json", "r", encoding="utf-8") as f:
+with open('ProjectStocks.json') as f:
     project_stocks = json.load(f)
+
 
 @app.route("/add_student", methods=["POST"])
 def add_student():
@@ -99,63 +98,85 @@ def sign_in():
     # Return a success message
     return jsonify({"message": f"Student {student_name} signed in successfully"}), 200
 
-@app.route("/invest_in_project", methods=["POST"])
-def invest_in_project():
-    # Get the student name, password, project name and points from the request body
-    student_name = request.json.get("student_name")
-    student_password = request.json.get("student_password")
-    project_name = request.json.get("project_name")
-    points = request.json.get("points")
-    # Check if the parameters are valid
-    if not student_name or not isinstance(student_name, str):
-        return jsonify({"error": "Invalid student name"}), 400
-    if not student_password or not isinstance(student_password, str):
-        return jsonify({"error": "Invalid student password"}), 400
-    if not project_name or not isinstance(project_name, str):
-        return jsonify({"error": "Invalid project name"}), 400
-    if not points or not isinstance(points, int) or points <= 0:
-        return jsonify({"error": "Invalid points"}), 400
-    # Check if the student exists in the student scores file
+
+@app.route('/invest', methods=['POST'])
+def invest():
+    # Parse request data
+    data = request.get_json()
+    student_name = data['student_name']
+    project_name = data['project_name']
+    invest_points = data['invest_points']
+
+    # Check if student exists and has enough points to invest
     if student_name not in student_scores:
-        return jsonify({"error": "Student not found"}), 404
-    # Check if the password matches the one in the student scores file
-    if student_password != student_scores[student_name]["password"]:
-        return jsonify({"error": "Wrong password"}), 401
-    # Check if the project exists in the project stocks file
+        return f"Error: {student_name} is not a valid student name"
+    if student_scores[student_name]['score'] < invest_points:
+        return f"Error: {student_name} does not have enough points to invest {invest_points}"
+
+    # Update project stock and student scores
     if project_name not in project_stocks:
-        return jsonify({"error": "Project not found"}), 404
-    # Check if the student has enough points to invest
-    if points > student_scores[student_name]["score"]:
-        return jsonify({"error": "Insufficient points"}), 403
-    # Deduct the points from the student score in the student scores file
-    student_scores[student_name]["score"] -= points
-    # Add the points to the project stock amount in the project stocks file
-    project_stocks[project_name]["stock_amount"] += points
-    # Update the project stock value based on a formula that depends on the stock amount and type of project
-    # For simplicity, I will use a linear formula: stock_value = base_value + factor * stock_amount / 1000
-    # The base value and factor are different for each type of project
-    base_value = {"programming": 5, "design": 10, "business": 15}
-    factor = {"programming": 2, "design": 1.5, "business": 1}
-    project_type = project_stocks[project_name]["type"]
-    project_stocks[project_name]["stock_value"] = base_value[project_type] + factor[project_type] * project_stocks[project_name]["stock_amount"] / 1000
-    # Round the stock value to two decimal places
-    project_stocks[project_name]["stock_value"] = round(project_stocks[project_name]["stock_value"], 2)
-    # Keep track of how many points each student invested in each project in a nested dictionary in the project stocks file
-    # The format is: {"investors": {"student_name": points, ...}, ...}
-    if "investors" not in project_stocks[project_name]:
-        project_stocks[project_name]["investors"] = {}
-    if student_name not in project_stocks[project_name]["investors"]:
-        project_stocks[project_name]["investors"][student_name] = 0
-    project_stocks[project_name]["investors"][student_name] += points
-    # Save the changes to both files
-    with open("student_scores.json", "w", encoding="utf-8") as f:
-        json.dump(student_scores, f, ensure_ascii=False)
-    with open("project_stocks.json", "w", encoding="utf-8") as f:
-        json.dump(project_stocks, f, ensure_ascii=False)
-    # Return a success message with the profit or loss information
-    if profit_or_loss > 0:
-        return jsonify({"message": f"Student {student_name} withdrew {points} points from project {project_name} and made a profit of {profit_or_loss} points"}), 200
-    elif profit_or_loss < 0:
-        return jsonify({"message": f"Student {student_name} withdrew {points} points from project {project_name} and made a loss of {abs(profit_or_loss)} points"}), 200
+        project_stocks[project_name] = {
+            'CEO': '', 'Type': '', 'StockValue': 0, 'StockAmmount': 0}
+    stock_amount = project_stocks[project_name]['StockAmmount']
+    stock_value = project_stocks[project_name]['StockValue']
+    new_stock_amount = stock_amount + invest_points
+    # TODO: Make the stock_value better go up when there are more investments and not tank down when someone takes their money out
+    stock_value = (stock_value * stock_amount +
+                   invest_points) / new_stock_amount
+    project_stocks[project_name]['StockAmmount'] = new_stock_amount
+    project_stocks[project_name]['StockValue'] = stock_value
+    student_scores[student_name]['score'] -= invest_points
+    if project_name not in student_scores[student_name]:
+        student_scores[student_name][project_name] = invest_points
     else:
-        return jsonify({"message": f"Student {student_name} withdrew {points} points from project {project_name} and broke even"}), 200
+        student_scores[student_name][project_name] += invest_points
+
+    # Write updated data to json files
+    with open('student_scores.json', 'w') as f:
+        json.dump(student_scores, f)
+    with open('ProjectStocks.json', 'w') as f:
+        json.dump(project_stocks, f)
+
+    return f"{student_name} invested {invest_points} points in {project_name}"
+
+
+@app.route('/withdraw', methods=['POST'])
+def withdraw():
+    # Parse request data
+    data = request.get_json()
+    student_name = data['student_name']
+    project_name = data['project_name']
+
+    # Check if student and project exist and if the student invested in the project
+    if student_name not in student_scores:
+        return f"Error: {student_name} is not a valid student name"
+    if project_name not in project_stocks:
+        return f"Error: {project_name} is not a valid project name"
+    if project_name not in student_scores[student_name]:
+        return f"Error: {student_name} did not invest in {project_name}"
+
+    # Calculate profit/loss based on stock value change
+    invested_points = student_scores[student_name][project_name]
+    stock_amount = project_stocks[project_name]['StockAmmount']
+    stock_value = project_stocks[project_name]['StockValue']
+    new_stock_amount = stock_amount - invested_points
+    if new_stock_amount == 0:
+        new_stock_value = 0
+    else:
+        new_stock_value = (stock_value * stock_amount -
+                           invested_points) / new_stock_amount
+    profit_loss = (new_stock_value - stock_value) * invested_points
+
+    # Update project stock and student scores
+    project_stocks[project_name]['StockAmmount'] = new_stock_amount
+    project_stocks[project_name]['StockValue'] = new_stock_value
+    student_scores[student_name]['score'] += invested_points
+    del student_scores[student_name][project_name]
+
+    # Write updated data to json files
+    with open('student_scores.json', 'w') as f:
+        json.dump(student_scores, f)
+    with open('ProjectStocks.json', 'w') as f:
+        json.dump(project_stocks, f)
+
+    return f"{student_name} withdrew {invested_points} points from {project_name} with a profit/loss of {profit_loss}"
